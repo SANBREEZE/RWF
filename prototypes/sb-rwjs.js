e824fd6eeb81df9244f8eb2232c07bd3c7ed33c4
SANBREEZE <developer@sanbreeze.com>
2014-09-09 01:37:39 +0200

    Update sb-rwjs.js

--- prototypes/sb-rwjs.js
+++ prototypes/sb-rwjs.js
@@ -45,20 +45,20 @@
 */
 
 (function($,fw){$.fn[fw]=function(fn){
-	var defs={
-		body:"body"
+	var defs = {
+		body : "body"
 	},opt;
 
-	var SB_RWJS=function(){
-		var v=["1.11.0","1.11.1"];
-		if(v.indexOf($.fn.jquery)==-1){
-			o="jQuery between version "+v[0]+" - "+v[v.length-1]+" is required!";
+	var SB_RWJS = function(){
+		var v = ["1.11.0", "1.11.1"];
+		if (v.indexOf($.fn.jquery) == -1) {
+			o = "jQuery between version " + v[0] + " - " + v[v.length-1] + " is required!";
 			console.log(o);
 			document.write(o);
 			return false;
-		}else{
-			$.fn.contextPopup=function(){
-				this.bind('contextmenu',function(e){
+		} else {
+			$.fn.contextPopup = function(){
+				this.bind('contextmenu', function(e){
 					return false;
 				});
 				return this;
@@ -70,8 +70,8 @@
 	}();
 
 	return {
-		setup:function(o){
-			opt=$.extend(defs,o);
+		setup : function(o){
+			opt = $.extend(defs, o);
 		}
 	}
-}})(jQuery,'SB_RWJS');
+}})(jQuery, 'SB_RWJS');
